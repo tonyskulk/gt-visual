3cd6af3889f850513dfbf73f80d4c98f2e25a87d
Tony Skulk <tony.skulk@gmail.com>
2018-03-20 00:18:39 +0100

    Initial project setup

---
+++ src/main/java/de/tonyskulk/gtvisual/GtVisualApplication.java
@@ -0,0 +1,41 @@
+package de.tonyskulk.gtvisual;
+
+import java.util.concurrent.CountDownLatch;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+@SpringBootApplication
+public class GtVisualApplication {
+
+  @Bean
+  public CountDownLatch closeLatch() {
+    return new CountDownLatch(1);
+  }
+
+  @Autowired
+  private MappingMongoConverter mongoConverter;
+
+  // Converts . into a mongo friendly char
+  @PostConstruct
+  public void setUpMongoEscapeCharacterConversion() {
+      mongoConverter.setMapKeyDotReplacement(",");
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    ConfigurableApplicationContext ctx = SpringApplication.run(GtVisualApplication.class, args);
+
+    final CountDownLatch closeLatch = ctx.getBean(CountDownLatch.class);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        closeLatch.countDown();
+      }
+    });
+    closeLatch.await();
+  }
+}
